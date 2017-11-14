6edfec850d0b60626815de6a802a9aa4ab5d498a
David Alexander <opensource@thelonelyghost.com>
2017-11-14 16:18:06 -0500

    Initial scaffolding

---
+++ config.rb
@@ -0,0 +1,129 @@
+ENV['WEBPACK_ENV'] ||= (build? ? 'build' : 'development')
+
+# Time.zone = "UTC"
+activate :external_pipeline,
+         name: :webpack,
+         command: if build?
+                    "WEBPACK_ENV=#{ENV.fetch('WEBPACK_ENV')} ./node_modules/webpack/bin/webpack.js --bail -p"
+                  else
+                    "WEBPACK_ENV=#{ENV.fetch('WEBPACK_ENV')} ./node_modules/webpack/bin/webpack.js --watch -d --progress --color"
+                  end,
+         source: '.tmp/dist',
+         latency: 1
+
+set :markdown_engine, :redcarpet
+set :markdown,
+    layout_engine: :erb,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    autolink: true,
+    disable_indented_code_blocks: true,
+    smartypants: true,
+    lax_spacing: true
+
+set :css_dir, 'assets/stylesheets'
+set :js_dir, 'assets/javascripts'
+set :images_dir, 'assets/images'
+
+set :site_name, 'Middleman'
+
+# Build-specific configuration
+configure :build do
+  set :trailing_slash, false
+
+  set :protocol, 'https'
+  set :host, 'www.example.com'
+  set :port, nil
+
+  set :google_analytics_id, 'UA-xxxxxxxx-x'
+
+  activate :asset_hash, ignore: [/^serviceworker.js/, /touch-icon.*png/]
+  activate :gzip, exts: %w(.js .css .html .htm .svg .ttf .otf .woff .eot)
+end
+
+configure :development do
+  set :protocol, 'http'
+  set :host, 'localhost'
+  set :port, 4567
+
+  set :google_analytics_id, 'UA-xxxxxxxx-x'
+end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+
+###
+# Helpers
+###
+
+# Automatic image dimensions on image_tag helper
+# activate :automatic_image_sizes
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# rubocop:disable Metrics/BlockLength
+helpers do
+  def phone_number(phone, linkify: false)
+    phone = phone.to_s.gsub('[^0-9]', '')[-10, 10]
+    label = format('(%s) %s-%s', phone[0...3], phone[3...6], phone[6..9])
+
+    if linkify
+      format('<a href="tel:+1%s">%s</a>', phone, label)
+    else
+      label
+    end
+  end
+
+  def page_title
+    yield_content(:title)
+  end
+
+  def page_header(title, summary: nil)
+    partial 'partials/page_header', locals: { title: title, summary: summary }
+  end
+
+  def section
+    (yield_content(:section) || title || '')
+  end
+
+  def to_url(opts = {})
+    Addressable::URI.new({
+      scheme: config[:protocol],
+      host: config[:host],
+      port: build? ? config[:port] : nil
+    }.merge(opts))
+  end
+
+  def current_url
+    path = current_page.path
+    path = '/' if current_page.path == 'index.html'
+    to_url(path: path)
+  end
+
+  def image_url(source)
+    to_url(path: image_path(source))
+  end
+
+  def email_url
+    format('mailto:%s', config[:contact_email])
+  end
+
+  def tweet_link_to(text, params = {})
+    uri = Addressable::URI.parse('https://twitter.com/intent/tweet')
+    uri.query_values = params
+    link_to text, uri, target: '_blank', rel: 'noopener'
+  end
+
+  def current_page_tags
+    Array(current_page.data[:tags])
+  end
+end
---
+++ webpack.config.js
@@ -0,0 +1,146 @@
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const Clean = require("clean-webpack-plugin");
+const path = require("path");
+
+var definePlugin = new webpack.DefinePlugin({
+  __DEVELOPMENT__: JSON.stringify(JSON.parse(process.env.WEBPACK_ENV === "development")),
+  __DEBUG__:       JSON.stringify(JSON.parse(process.env.WEBPACK_ENV === "debug")),
+  __BUILD__:       JSON.stringify(JSON.parse(process.env.WEBPACK_ENV === "build")),
+  __VERSION__:     (new Date().getTime().toString())
+});
+
+var siteConfig = {
+  entry: {
+    index: [
+      "./source/assets/stylesheets/index.scss",
+      "./source/assets/javascripts/index.js"
+    ],
+    head: "./source/assets/javascripts/head.js",
+  },
+
+  resolve: {
+    modules: [
+      path.join(__dirname, "source", "assets", "javascripts"),
+      "node_modules"
+    ]
+  },
+
+  output: {
+    path: __dirname + "/.tmp/dist",
+    filename: "assets/javascripts/[name].bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /source\/assets\/javascripts\/.*\.js$/,
+        exclude: /(node_modules|\.tmp|vendor)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "es2015", "stage-0"],
+          }
+        },
+      },
+
+      {
+        test: require.resolve("jquery"),
+        use: [{
+          loader: "expose-loader",
+          options: "$"
+        }]
+      },
+
+      {
+        test: /[\\/]vendor[\\/]modernizr\.js$/,
+        use: [
+          "imports-loader?this=>window",
+          "exports-loader?Modernizr"
+        ]
+      },
+
+      {
+        test: /(\.css|\.scss)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+                includePaths: [
+                  path.resolve(__dirname, "node_modules/foundation-sites"),
+                  path.resolve(__dirname, "node_modules/highlight.js/styles"),
+                ]
+              }
+            }
+          ]
+        })
+      },
+    ],
+  },
+
+  node: {
+    console: true
+  },
+
+  plugins: [
+    definePlugin,
+    new Clean([".tmp"]),
+    new ExtractTextPlugin("assets/stylesheets/index.bundle.css"),
+    new webpack.optimize.CommonsChunkPlugin({name: "head", filename: "assets/javascripts/head.bundle.js"}),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    }),
+  ],
+};
+
+var swConfig = {
+  entry: {
+    serviceworker: "./source/assets/javascripts/serviceworker.js",
+  },
+
+  resolve: {
+    modules: [
+      path.join(__dirname, "source", "assets", "javascripts"),
+      "node_modules"
+    ]
+  },
+
+  output: {
+    path: __dirname + "/.tmp/dist",
+    filename: "serviceworker.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /source\/assets\/javascripts\/.*\.js$/,
+        exclude: /(node_modules|\.tmp|vendor)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env", "es2015", "stage-0"],
+          }
+        },
+      },
+    ],
+  },
+
+  node: {
+    console: true
+  },
+
+  plugins: [definePlugin],
+};
+
+module.exports = [ siteConfig, swConfig ];
